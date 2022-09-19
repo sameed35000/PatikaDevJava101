8ea72766437aaf642e93c2e062ae2b39b480be6d
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-19 19:00:12 +0300

    %18'lik KDV Tutarı Hesaplayan Program

--- KdvHesaplayanProgram/src/KdvHesaplama.java
+++ KdvHesaplayanProgram/src/KdvHesaplama.java
@@ -1,4 +1,15 @@
-
+import java.util.Scanner;
 public class KdvHesaplama {
+	public static void main(String[] args) {
+		double fiyat, kdvliFiyat, kdvTutarı;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Ürünün Fiyatını Giriniz: ");
+		fiyat = input.nextDouble();
+		kdvliFiyat = fiyat + (fiyat * 0.18);
+		kdvTutarı = kdvliFiyat - fiyat;
+		System.out.println("Ürünün Fiyatı: " + fiyat);
+		System.out.println("Ürünün KDV'li Fiyatı: " + kdvliFiyat);
+		System.out.println("KDV Tutarı: " + kdvTutarı);
+	}
 
 }
