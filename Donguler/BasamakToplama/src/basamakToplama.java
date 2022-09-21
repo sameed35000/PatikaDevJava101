cb69e71e00a118c3df3bd711de4132f4a76f7927
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-21 19:23:06 +0300

    Girilen Sayıyı Basamaklarına Ayırıp Basamakları Toplayan Program

---
+++ Donguler/BasamakToplama/src/basamakToplama.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class basamakToplama {
+
+	public static void main(String[] args) {
+		int num, basSayisi = 0, sum = 0;
+		int k = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Lütfen Bir Sayi Giriniz: ");
+		num = input.nextInt();
+		int tempNum = num;
+		
+		while(tempNum != 0) {
+			tempNum /= 10;
+			basSayisi++;
+		}
+		tempNum = num;
+		for(int i = 1; i <= basSayisi; i++) {
+			k = tempNum % 10;
+			tempNum /= 10;
+			sum += k;
+			if(i < basSayisi) {
+				System.out.print(k + " + ");
+			}else System.out.print(k);
+			
+		}
+		System.out.print(" = " + sum);
+
+	}
+
+}
