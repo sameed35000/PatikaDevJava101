768488988f624a4cdec7f8d2865e7d71b617ddb1
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-21 20:29:56 +0300

    Girilen Sayıya Göre Yıldızlardan Üçgen Yapan Program

--- Donguler/StarDiamond/src/starDiamond.java
+++ Donguler/StarDiamond/src/starDiamond.java
@@ -6,27 +6,26 @@ public class starDiamond {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Bir Sayi Giriniz: ");
 		int n = input.nextInt();
-		
-		for (int i = 0; i <= n-1; i++) {
-			for (int j = 0; j <= n-i; j++) {
+
+		for (int i = 1; i <= n ; i++) {
+			for (int j = 1; j <= (n - i); j++) {
 				System.out.print(" ");
 			}
-			for (int k = 1; k <= 2*i+1 ;k++) {
+			for (int k = 1; k <= (2 * i - 1); k++) {
 				System.out.print("*");
 			}
 			System.out.print("\n");
 		}
-		for (int i = n-1; i >= 0; i--) {
-			for (int j = n-i; j >= 0; j--) {
+		for (int i = n - 1; i >= 1; i--) {
+			for (int j = (n - i); j >= 1; j--) {
 				System.out.print(" ");
 			}
-			for (int k = 2*i+1; k >= 1 ;k--) {
+			for (int k = (2 * i - 1); k >= 1; k--) {
 				System.out.print("*");
 			}
 			System.out.print("\n");
 		}
+		input.close();
 	}
 
 }
-
-
