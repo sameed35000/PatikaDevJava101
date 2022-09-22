c58c1cd027844a836c2317624c378a2e12aa5e85
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-22 23:39:11 +0300

    Fibonacci Serisi Yazdıran Program

---
+++ Donguler/Fibonacci/src/fibonacci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		int num, fibOne = 0, fibTwo = 1;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Fibonacci Serisinde Kaç Sayı Görmek İstiyorsunuz?: ");
+		num = input.nextInt();
+		for (int i = 0; i <= num; i++) {
+			int total = 0;
+			if (i == 0) {
+				System.out.println(fibOne + " + " + fibTwo + " = " + fibTwo);
+			} else if (i % 2 == 1) {
+				fibOne = fibOne + fibTwo;
+				total = fibOne + fibTwo;
+				System.out.println(fibTwo + " + " + fibOne + " = " + total);
+			} else if (i % 2 == 0) {
+				fibTwo = fibOne + fibTwo;
+				total = fibOne + fibTwo;
+				System.out.println(fibOne + " + " + fibTwo + " = " + total);
+			}
+			input.close();
+		}
+	}
+}
+
+
+
