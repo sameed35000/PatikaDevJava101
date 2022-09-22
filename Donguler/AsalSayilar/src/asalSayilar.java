f471911d376e26792eceaf3569e9f03e04f3b038
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-22 22:51:05 +0300

    1'den 100'e Kadar Olan Asal Sayıları Listeleme

---
+++ Donguler/AsalSayilar/src/asalSayilar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class asalSayilar {
+
+	public static void main(String[] args) {
+		
+        boolean isPrime;
+
+        for (int sayi=2; sayi<100;sayi++){
+            isPrime=true;
+
+            for(int i=2; i<sayi; i++){
+                if(sayi % i== 0){
+                    isPrime=false;
+                }
+            }
+            if(isPrime){
+                System.out.print(sayi + " ");
+            }
+        }
+
+	}
+
+}
