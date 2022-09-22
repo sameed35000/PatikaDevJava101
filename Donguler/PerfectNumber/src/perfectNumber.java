3d4c60c965b589a77e3c0aca7874465b753a7221
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-22 22:29:36 +0300

    Mükemmel Sayıları Bulan Program

---
+++ Donguler/PerfectNumber/src/perfectNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class perfectNumber {
+
+	public static void main(String[] args) {
+		int number, total = 0;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Sayınızı Giriniz: ");
+		number = input.nextInt();
+
+		for (int i = number - 1; i > 0; i--) {
+			if (number % i == 0) {
+				total += i;
+			}
+		}
+		if (total == number) {
+			System.out.println("Girdiğiniz " + number + " Sayısı Mükemmel Sayıdır.");
+		}
+		else {
+			System.out.println("Girdiğiniz " + number + " Sayısı Mükemmel Sayı Değildir.");
+		}
+
+	}
+
+}
