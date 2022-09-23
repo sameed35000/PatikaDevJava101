3151668d1d90b4a4e7bbb19f0a317a75d70b5b17
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-23 09:48:04 +0300

    Metotlar

---
+++ Metotlar/ders/src/ders/ders.java
@@ -0,0 +1,17 @@
+package ders;
+
+public class ders {
+	
+	public static void main(String[] args) {
+        recursiveMethod(4);
+    }
+
+    static void recursiveMethod(int num) {
+        num--;
+        if (num == 0)
+            return;
+        System.out.print(num + ",");
+        recursiveMethod(num);
+    }
+
+}
