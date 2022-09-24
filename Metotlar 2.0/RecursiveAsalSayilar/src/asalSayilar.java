ac98099e6e70a5655a141d0a4a6b9e3d85f7c081
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-24 09:09:19 +0300

    Recursive Metotla Asal Sayı Sorgulaması

---
+++ Metotlar 2.0/RecursiveAsalSayilar/src/asalSayilar.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class asalSayilar {
+
+	static String isPrime(int number, int i) {
+
+		if (number <= 1) {
+			String str = " Sayısı Bir Asal Sayı Değildir.";
+			return (number + str);
+		}
+		if (i * i > number) {
+			String str = " Sayısı Bir Asal Sayıdır";
+			return (number + str);
+		}
+		if (number % i == 0) {
+			String str = " Sayısı Bir Asal Sayı Değildir.";
+			return (number + str);
+		}
+		return isPrime(number, i + 1);
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Lütfen Sorgulanacak Sayıyı Giriniz: ");
+		int number = input.nextInt();
+		System.out.println(isPrime(number, 2));
+
+	}
+
+}
