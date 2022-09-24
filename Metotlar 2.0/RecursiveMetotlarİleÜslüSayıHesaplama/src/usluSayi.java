8faca20d19c7378f5d330dcfd85030b1fac47273
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-24 07:11:52 +0300

    Recursive Metotlarla Üslü Sayı Hesaplama

---
+++ Metotlar 2.0/RecursiveMetotlarİleÜslüSayıHesaplama/src/usluSayi.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class usluSayi {
+
+	static int us(int taban, int us) {
+		int sonuc = 1;
+		if (us == 0) {
+			return 1;
+		} else if (taban == 0) {
+			return 0;
+		} else {
+			while (us != 0) {
+				sonuc *= taban;
+				us--;
+			}
+		}
+		return sonuc;
+	}
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Lütfen Taban Değerini Giriniz: ");
+		int taban = input.nextInt();
+		System.out.print("Lütfen Üs Değerini Giriniz: ");
+		int us = input.nextInt();
+		System.out.println("Sonuç: " + us(taban, us));
+		input.close();
+	}
+}
