cc9366c4270e80f346a23b1651bb4b0eb8701bda
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-26 12:15:50 +0300

    Diziler Klasörü Oluşturuldu

---
+++ Diziler/DiziHarmonikOrtalamasi/src/diziOrtalama.java
@@ -0,0 +1,16 @@
+public class diziOrtalama {
+
+	public static void main(String[] args) {
+		int[] list = { 1, 3, 5, 7, 9 };
+		double sum = 0;
+
+		for (double i: list) {
+			sum += (1.0/i);
+			System.out.println(i);
+		}
+		System.out.println("Array'in Elemanlarının Ortalaması: " + (list.length/sum));
+		
+
+	}
+
+}
