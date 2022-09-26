6d5bd38f258055d7ec335b6f9831fefa1d2d1946
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-26 16:22:09 +0300

    Arraylerde Girilen Sayıya En Yakın Sayıları Bulma

---
+++ Diziler/ArraylerdeEnYakinMinMax/src/NearestMinMax.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class NearestMinMax {
+
+	public static void main(String[] args) {
+		int[] list = { 15, 12, 788, 1, -1, -778, 2, 0 };
+		Arrays.sort(list);
+		Scanner input = new Scanner(System.in);
+		System.out.print("Bir Sayı Giriniz: ");
+		int x = input.nextInt();
+		for (int i = 0; i < list.length; i++) {
+			if (list[i] >= x) {
+				System.out.println("Girdiğiniz Sayıya En Yakın Kendisinden Küçük Sayı: " + list[i - 1]);
+				System.out.println("Girdiğiniz Sayıya En Yakın Kendisinden Büyük Sayı: " + list[i]);
+				break;
+			}
+		}
+		input.close();
+		
+	}
+}
