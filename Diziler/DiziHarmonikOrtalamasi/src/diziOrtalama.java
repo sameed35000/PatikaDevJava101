d7d4654d799a61d7301dc39b2b809194043c2d8b
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-26 12:16:29 +0300

    Dizi Elemanlarının Harmonik Ortalamasını Bulma

--- Diziler/DiziHarmonikOrtalamasi/src/diziOrtalama.java
+++ Diziler/DiziHarmonikOrtalamasi/src/diziOrtalama.java
@@ -4,13 +4,11 @@ public class diziOrtalama {
 		int[] list = { 1, 3, 5, 7, 9 };
 		double sum = 0;
 
-		for (double i: list) {
-			sum += (1.0/i);
+		for (double i : list) {
+			sum += (1.0 / i);
 			System.out.println(i);
 		}
-		System.out.println("Array'in Elemanlarının Ortalaması: " + (list.length/sum));
 		
-
+		System.out.println("Array'in Elemanlarının Ortalaması: " + (list.length / sum));
 	}
-
 }
