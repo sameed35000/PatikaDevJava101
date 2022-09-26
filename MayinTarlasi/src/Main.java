e34341b746526f951b2f66184082602271240b9d
Samed Er <sameed35000@icloud.congit config --global user.email sameed35000@icloud.com>
2022-09-27 01:11:16 +0300

    Mayın Tarlası Oyunu

---
+++ MayinTarlasi/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int row , column ;
+        System.out.print("Satır Sayısını Giriniz: ");
+        row = scanner.nextInt();
+        System.out.print("Sütun Sayısını Giriniz: ");
+        column = scanner.nextInt();
+        MineSweeper mineSweeper = new MineSweeper(row, column);
+        mineSweeper.run();
+        scanner.close();
+	}
+}
